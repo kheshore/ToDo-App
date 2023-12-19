f78c0c3a17f63d80464fcb2316dc7b5329c9bd29
kheshore <kheshore@gmail.com>
2023-12-19 21:40:23 +0530

    Version 1 [can add todos, delete and checkbox it]

--- src/AddToDo.js
+++ src/AddToDo.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { RiAddCircleFill } from "react-icons/ri";
 
-const AddToDo = (newItem, setNewItem) => {
+// AddToDo component to add new todo
+// newTodo and setNewTodo are props from App passed as a parameter of object
+// handleSubmit is a function from App
+// RiAddCircleFill is an icon
+const AddToDo = ({newTodo, setNewTodo, handleSubmit}) => {
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}> 
         <input 
         type="text"
         placeholder='Add Item'
-        id='addItem'
         required
         autoFocus
-        value={newItem}
-        onChange = {(e)=> setNewItem(e.target.value)}
+        // this is a controlled component
+        // controlled component is when the value is controlled by the user
+        // newTodo is value and setNewTodo is a function with value as parameter
+        // here value is set to {newTodo}
+        // onChange is an event where the input value is set when the user types
+        // e.target.value is the value of the input field
+        value={newTodo}
+        onChange = {(e)=> setNewTodo(e.target.value)}
         />
         <button
         type='submit'
--- src/App.js
+++ src/App.js
@@ -5,40 +5,82 @@ import Footer from './Footer';
 import { useState } from "react";
 import AddToDo from "./AddToDo"
 
+// Main App Function
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      checked: true,
-      todo: "Learn React",
-    },
-    {
-      id: 2,
-      checked: false,
-      todo: "Master React",
-    },
-  ]);
-  const [newItem, setNewItem] = useState('')
 
+  // state to store todos in local storage
+  // todos is an array
+  // setTodos is a function to store todos
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('todos')) || []
+  )
+
+  // state to store new todo
+  // newTodo is a string because it is an input
+  // setNewTodo is a function to store new todo
+  const [newTodo, setNewTodo] = useState('')
+
+  // addTodo is a function to add new todo
+  // addNewTodo is an object
+  // newTodo is a string with the value of old todos and new todo
+  // ...todos is an array of old todos
+  // ... is used to spread the old todos and add new todo with it
+  // todo is an object from the input
+  const addTodo = (todo) => {
+    //for unique ID
+    const id = todos.length  ? todos[todos.length -1].id + 1 : 1 ;
+    //create new item
+    const addNewTodo = {
+      id,checked:false,todo 
+    }
+    // creates new array and stores the new todo along with the old todos
+    const newTodo = [...todos, addNewTodo]
+    setTodos(newTodo)
+    localStorage.setItem('todos', JSON.stringify(newTodo))
+  }
+
+  // addTodo is a function (above)
+  // e.preventDefault() is used to prevent the form from submitting (reloading)
+  // setNewTodo is a function to make the input empty
+  // newTodo is a string passed from the input to addTodo as a parameter
   const handleSubmit = (e) => {
-    setnewItem(newItem)
+    e.preventDefault();
+    addTodo(newTodo)
+    setNewTodo('')
   }
-  const handleToggle = (id) => {
-    setTodos(todos.map((todo) => todo.id === id ? {...todo, checked:!todo.checked} : todo))
-  };
 
-  const handleDelete = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
+  // handleToggle is a function to change the checked value (tick box)
+  // id is a parameter passed from the Content component
+  // setTodos is a function to store todos after alteration
+  // .map is used to loop through the todos
+  const handleToggle = (id) => {
+    const listTodos = todos.map((todo) => todo.id === id ? {...todo, checked:!todo.checked} : todo)
+    setTodos(listTodos)
+    localStorage.setItem('todos', JSON.stringify(listTodos))
   }
+
+  // handleDelete is a function to delete the todo
+  // id is a parameter passed from the Content component
+  // .filter is used to filter out the todos that are not equal to the id
+  // setTodos is a function to store todos after deletion
+  const handleDelete = (id) => {
+    const listTodos = todos.filter((todo) => todo.id !== id)
+    setTodos(listTodos)
+    localStorage.setItem('todos', JSON.stringify(listTodos))
+  }
+
+  // count is the number of todos
   const count = todos.length;
+  
+  // return statement for App
   return (
     <div className="App">
       <Header 
       //title = "List" for props (child to parent)
       />
       <AddToDo
-      newItem = {newItem}
-      setNewItem = {setNewItem}
+      newTodo = {newTodo}
+      setNewTodo = {setNewTodo}
       handleSubmit = {handleSubmit}
       />
       
