7b19228d7ef8726a4e5d4aa8d176610434471afc
Kheshore J R <43311731+kheshore@users.noreply.github.com>
2023-12-20 17:08:02 +0530

    V2.2
    Signed-off-by: Kheshore J R <43311731+kheshore@users.noreply.github.com>

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { FaListUl } from "react-icons/fa";
+
 
 const Footer = ({count}) => {
   return (
    <footer>
+    <FaListUl 
+    style={{marginRight: "5px", fontSize: "18px"}}
+    />
     <p>List count: {count}</p>
    </footer>
   )
